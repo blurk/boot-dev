38c578d84a2074e5dc29ff6413a0b461952e37fb
blurk <nsinh6745@gmail.com>
2025-03-19 22:14:28 +0700

    go: chapter 4

---
+++ 013-learn-go/ch04/c2-send-message/main.go
@@ -0,0 +1,32 @@
+package main
+
+func (U User) SendMessage(message string, messageLength int) (string, bool) {
+	if messageLength > U.MessageCharLimit {
+		return "", false
+	}
+
+	return message, true
+}
+
+// don't touch below this line
+
+type User struct {
+	Name string
+	Membership
+}
+
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
+func newUser(name string, membershipType string) User {
+	membership := Membership{Type: membershipType}
+	if membershipType == "premium" {
+		membership.MessageCharLimit = 1000
+	} else {
+		membership.Type = "standard"
+		membership.MessageCharLimit = 100
+	}
+	return User{Name: name, Membership: membership}
+}
